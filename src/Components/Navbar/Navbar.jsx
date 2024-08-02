3bc53370b535ec1356d88c2420d16fa67eaf89e1
nedaaelsayed165 <nedaaelsayed321@gmail.com>
2024-08-02 18:05:40 +0300

    ref: move logo to public dir

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -9,11 +9,11 @@ const Navbar = () => {
     <div className="flex justify-between items-center border-b border-gray-100 w-full px-20 py-1">
       <Link to="/">
         <div className="text-lg font-semibold text-gray-900 dark:text-white font-roboto justify-between flex gap-2">
-          <img src="/images/logo1.jpg" className="w-12 flex-row" alt="" />
+          <img src="/logo1.jpg" className="w-12 flex-row" alt="" />
           <span className="text-transparent bg-clip-text bg-gradient-to-r to-red-600 from-blue-400  ">
             Free Palestine
           </span>{" "}
-          <img src="/images/logo1.jpg" className="w-12 " alt="" />
+          <img src="/logo1.jpg" className="w-12 " alt="" />
         </div>
       </Link>
       <div className="flex justify-center item-center mx-auto">
